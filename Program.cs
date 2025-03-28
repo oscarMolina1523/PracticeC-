f9d8d7bd7ad271b2b26f41f38f7de4b04afaf179
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-03-28 14:34:43 -0600

    Initial configuration

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+  static int Main(string[] args)
+  {
+    StringBuilder builder = new();
+    builder.AppendLine("The following arguments are passed:");
+
+    foreach (var arg in args)
+    {
+      builder.AppendLine($"Argument={arg}");
+    }
+
+    Console.WriteLine(builder.ToString());
+
+    return 0;
+  }
+}
